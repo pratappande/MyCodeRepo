dc0ef31726ff9b99cf3c473eba9e194f2cf55018
Pratap Pande <pratappande@gmail.com>
2025-11-22 18:29:39 +0530

    Create Sum of Odd indexed elements in a range.java

---
+++ Intermediate DSA/Sum of Odd indexed elements in a range.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int[] sumOfOddIndexedElements(int[] A, int[][] B) {
+        int n = A.length;
+        int m = B.length;
+
+        int[] Arr = new int[n];
+        Arr[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (i%2 == 1) {
+                Arr[i] = Arr[i-1] + A[i];
+            } else {
+                Arr[i] = Arr[i-1];
+            }
+        }
+
+        int[] result = new int[m];
+        for (int i = 0; i < m; i++) {
+            int l = B[i][0];
+            int r = B[i][1];
+            
+            if (l == 0) {
+                result[i] = Arr[r];
+            } else {
+                result[i] = Arr[r] - Arr[l-1];
+            }
+        }
+        return result;
+    }
+}
