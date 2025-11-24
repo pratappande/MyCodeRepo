d5efad4f8b02b9a12a99a7e52448ac98ab1b58c5
Pratap Pande <pratappande@gmail.com>
2025-11-24 21:37:03 +0530

    Create Sum of All Subarrays.java

---
+++ Intermediate DSA/Sum of All Subarrays.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public long subarraySum(int[] A) {
+        long ans = 0;
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            ans += (long) (i+1)*(n-i)*A[i];
+        }
+        return ans;
+    }
+}
