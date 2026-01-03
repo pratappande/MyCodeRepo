f42aa19af6b32478c56c257583b953e35c870ab9
Pratap Pande <pratappande@gmail.com>
2026-01-03 11:46:16 +0530

    Create Matrix Scalar Product.java

---
+++ Intermediate DSA/Matrix Scalar Product.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[][] solve(int[][] A, int B) {
+        int n = A.length;
+        int m = A[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                A[i][j] = A[i][j] * B;
+            }
+        }
+        return A;
+    }
+}
