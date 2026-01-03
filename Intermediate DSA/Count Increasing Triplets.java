ec76f8f8197ab32dca032db0013694569a5caaf0
Pratap Pande <pratappande@gmail.com>
2026-01-03 11:52:44 +0530

    Create Count Increasing Triplets.java

---
+++ Intermediate DSA/Count Increasing Triplets.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int solve(int[] A) {
+        int n = A.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k< n; k++) {
+                    if (A[i]<A[j] && A[j]<A[k]) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
