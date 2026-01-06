492ea9476f76b4e542e78f989d4609d31e034152
Pratap Pande <pratappande@gmail.com>
2026-01-06 20:16:39 +0530

    Update Number of 1 Bits.java

--- Advanced DSA 1/Number of 1 Bits.java
+++ Advanced DSA 1/Number of 1 Bits.java
@@ -1,13 +1,9 @@
 public class Solution {
     public int numSetBits(int A) {
-        String s = Integer.toBinaryString(A);
-        int n = s.length();
         int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == '1') {
-                count++;
-            }
+        while (A != 0) {
+            A = A & (A-1);
+            count++;
         }
         return count;
     }
